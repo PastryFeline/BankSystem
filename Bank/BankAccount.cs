6233d3e4e9f5547b902beb7ffdd140c1e10cb298
Rasmus Mikkel Cæsarsen <rasm639q@elev.techcollege.dk>
2022-10-14 07:59:42 +0200

    Add project files.

---
+++ Bank/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class BankAccount
+    {
+        private double balance;
+    
+
+
+    public double PrintBal()
+        {
+            return balance;
+        }
+    public void Deposit(double amount)
+        {
+            balance +=  amount;
+            Console.WriteLine($"{amount} has now been added to balance!!");
+        }
+    public void Withdraw(double amount)
+        {
+            balance -= amount;
+            Console.WriteLine($"{amount} has now been withrawed from balance!!!");
+        }
+
+
+    }
+
+}
---
+++ Bank/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Bank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new();
+            Console.WriteLine(account.PrintBal());
+            account.Deposit(1000);
+            Console.WriteLine(account.PrintBal());
+            account.Withdraw(100);
+            Console.WriteLine(account.PrintBal());
+
+        }
+    }
+}
